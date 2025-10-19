c24fd8bddd3e4acfda213c3db40523c427f15ce1
Sumit kumar <sumitkumar808393@gmail.com>
2025-10-19 21:23:58 +0530

    First

--- src/main/java/com/Springcore/DemoProject/DemoController.java
+++ src/main/java/com/Springcore/DemoProject/DemoController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoController {
 	
-	@GetMapping("/Welcome3")
+	@GetMapping("/Welcome34")
 	public String DemoCon() {
 		return "This is For Server Check";
 	}
